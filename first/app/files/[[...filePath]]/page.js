123aac8cf98c620849bb66a8e5ef33c517380c4d
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-09-01 06:57:29 +0530

    catching all routes

---
+++ first/app/files/[[...filePath]]/page.js
@@ -0,0 +1,8 @@
+export default async function FileComponent({params}){
+    const { filePath } = await params;
+    
+    return(
+        
+        <div><h1>File <i>{filePath.join("/")}</i></h1></div>
+    )
+}

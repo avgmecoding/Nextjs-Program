623bf8f62eafedb4a7955f49bd3a161125cb8ad7
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-09-14 22:20:48 +0530

    built reusable layouts

---
+++ first/app/services/app/layout.js
@@ -0,0 +1,5 @@
+export default function ServicesLayout({childern}){
+    return(
+        <div>{childern}</div>
+    )
+}
--- first/app/services/page.js
+++ first/app/services/page.js
@@ -5,8 +5,11 @@ export default function Services(){
         <>
             <h1>All Services</h1>
             <Link href="/services/app">App Development</Link>
+            <br />
             <Link href="/services/web">Web Development</Link>
+            <br />
             <Link href="/sevices/seo">Search Engine Optimization</Link>
+            <br />
             <Link href="/services/desktop">Desktop App Dev</Link>
         </>
         

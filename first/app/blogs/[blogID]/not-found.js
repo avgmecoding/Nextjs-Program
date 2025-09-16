ee07377b2ed487310aa1fd00e76d228ee93e2b31
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-09-16 14:10:30 +0530

    created custom not found pages with dynamic routing

---
+++ first/app/blogs/[blogID]/not-found.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { usePathname } from "next/navigation"
+
+export default function BlogNotFound() {
+  const a = usePathname()
+  console.log(a);
+  
+  return (
+    <div>
+      <h1>{a.split()} Blog page not found!!</h1>
+      <p>Couldn't found the page you are looking for....</p>
+    </div>
+  )
+}
+
+
---
+++ first/app/not-found.js
@@ -0,0 +1,11 @@
+
+export default function () {
+  return (
+    <div>
+      <h1>Page not found!!</h1>
+      <p>Couldn't found the page you are looking for....</p>
+    </div>
+  )
+}
+
+

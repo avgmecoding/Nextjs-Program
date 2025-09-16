924032cb68f7010cbec4636aaeacc2cf479d2e0c
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-09-16 13:42:15 +0530

    set blogID type using regex

--- first/app/blogs/[blogID]/page.js
+++ first/app/blogs/[blogID]/page.js
@@ -1,13 +1,19 @@
-export async function generateMetadata({params}){
-    const {blogID} = await params;
-    return{
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+    const { blogID } = await params;
+    return {
         title: `Blog ${blogID}`,
     }
 }
 
-export default async function Blog1({params}){
-    const {blogID} = await params;
-    return(
+export default async function Blog1({ params }) {
+    const { blogID } = await params;
+    if (!/^\d+$/.test(blogID)
+    ) {
+        notFound();
+    }
+    return (
         <h1>Welcome to blog <b>{blogID}</b></h1>
     )
 }
